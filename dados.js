c9e67e2b95cfee6bbd2d9f047f757aa8ba0e7586
Rodrigo Lima <radrigo32@gmail.com>
2024-09-06 15:09:40 -0300

    projeto finalizado

---
+++ dados.js
@@ -0,0 +1,74 @@
+let dados = [
+        {
+            titulo: "Simão Pedro",
+            descricao: "Líder dos apóstolos, conhecido por sua fé e impulsividade. Negou Jesus três vezes, mas depois se arrependeu e se tornou um pilar da Igreja primitiva.",
+            link: "https://pt.wikipedia.org/wiki/Pedro_(ap%C3%B3stolo)",
+            tags: "lider pescador rocha fe pedro"
+        },
+        {
+            titulo: "André",
+            descricao: "Irmão de Pedro, um dos primeiros discípulos de Jesus. Conhecido por sua fé e por apresentar seu irmão a Jesus.",
+            link: "https://pt.wikipedia.org/wiki/Andr%C3%A9,_o_Ap%C3%B3stolo",
+            tags: "discipulo pescador evangelista andre"
+        },
+        {
+            titulo: "Tiago, filho de Zebedeu",
+            descricao: "Conhecido como o 'Boanerges' (filho do trovão), junto com seu irmão João. Um dos primeiros discípulos de Jesus.",
+            link: "https://pt.wikipedia.org/wiki/Santiago_Maior",
+            tags: "boanerges filho-do-trovao apostolo tiago thiago"
+        },
+        {
+            titulo: "João",
+            descricao: "O discípulo amado de Jesus, autor de um dos evangelhos e de três cartas. Conhecido por sua profunda espiritualidade.",
+            link: "https://pt.wikipedia.org/wiki/Jo%C3%A3o,_o_Ap%C3%B3stolo",
+            tags: "discipulo-amado evangelista apostolo joao"
+        },
+        {
+            titulo: "Filipe",
+            descricao: "Um dos primeiros discípulos de Jesus, conhecido por sua fé e por apresentar os gregos a Jesus.",
+            link: "https://pt.wikipedia.org/wiki/Filipe_(ap%C3%B3stolo)",
+            tags: "discipulo evangelista felipe "
+        },
+        {
+            titulo: "Bartolomeu",
+            descricao: "Identificado por alguns como Natanael. Conhecido por sua sinceridade e fé.",
+            link: "https://pt.wikipedia.org/wiki/Bartolomeu,_o_Ap%C3%B3stolo",
+            tags: "discipulo natanael"
+        },
+        {
+            titulo: "Tomé",
+            descricao: "Conhecido por duvidar da ressurreição de Jesus até vê-lo pessoalmente.",
+            link: "https://pt.wikipedia.org/wiki/Tom%C3%A9,_o_Ap%C3%B3stolo",
+            tags: "discipulo duvida tome"
+        },
+        {
+            titulo: "Mateus",
+            descricao: "Um cobrador de impostos que se tornou apóstolo e escreveu um dos evangelhos.",
+            link: "https://pt.wikipedia.org/wiki/Mateus,_o_Evangelista",
+            tags: "matheus mat"
+        },
+        {
+            titulo: "Tiago, filho de Alfeu",
+            descricao: "Menos conhecido, mas um dos doze apóstolos.",
+            link: "https://pt.wikipedia.org/wiki/Tiago,_filho_de_Alfeu",
+            tags: "thiago"
+        },
+        {
+            titulo: "Judas Tadeu",
+            descricao: "Também conhecido como Judas Lebeu.",
+            link: "https://pt.wikipedia.org/wiki/Judas_Tadeu",
+            tags: "judas tadeu"
+        },
+        {
+            titulo: "Simão, o Zelote",
+            descricao: "Um membro do partido zelota, que buscava a libertação de Israel do domínio romano.",
+            link: "https://pt.wikipedia.org/wiki/Sim%C3%A3o,_o_Zelote",
+            tags: "simao zelotte"
+        },
+        {
+            titulo: "Judas Iscariotes",
+            descricao: "O apóstolo que traiu Jesus.",
+            link: "https://pt.wikipedia.org/wiki/Judas_Iscariotes",
+            tags: "traidor judas beijou "
+        }
+    ];
